1bf651096de61b973aa821466fa440d6ac09d6a0
Hernan Jurado <Hernan.jurado@udla.edu.ec>
2023-10-18 13:32:20 -0500

    Creacion

---
+++ HJ_Burger_BDD/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HJ_Burger_BDD.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
